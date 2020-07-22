22f383382326100fcf3da6c7dba3af1528ad40dd
slavkluev <slavkluev@yandex.ru>
2020-07-22 09:03:30 +0700

    Refactoring

--- src/commands/Ip2LocationController.php
+++ src/commands/Ip2LocationController.php
@@ -4,19 +4,27 @@ declare(strict_types=1);
 
 namespace slavkluev\Ip2Location\commands;
 
+use Yii;
 use yii\console\Controller;
+use yii\console\ExitCode;
+use yii\helpers\Console;
 
 class Ip2LocationController extends Controller
 {
     public $defaultAction = 'update';
 
+    /**
+     * Updates and reloads the database.
+     */
     public function actionUpdate()
     {
         try {
-            \Yii::$app->ip2location->update();
-            echo 'Database was updated successfully.' . PHP_EOL;
+            Yii::$app->ip2location->update();
+            $this->stdout('Database was updated successfully.', Console::FG_GREEN);
+            return ExitCode::OK;
         } catch (\Exception $exception) {
-            echo $exception->getMessage() . PHP_EOL;
+            $this->stderr($exception->getMessage(), Console::FG_RED);
+            return ExitCode::UNSPECIFIED_ERROR;
         }
     }
 }
